33e1823bd98c594b8ef33767c0cfb0b164e8fe1f
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-03-07 13:42:22 -0500

    ✨ feat: init project

---
+++ src/components/MessageText.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1 class="message">{{ msg }}</h1>
+</template>
+
+<style scoped>
+.message {
+  color: #bbb;
+  background-color: #222;
+}
+</style>
